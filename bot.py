242ec7a4e9fb8ae7d80571f58234ab026a97c1d6
SimJunYou <simjunyou99@gmail.com>
2022-07-01 01:30:47 +0800

    Added logging and separated start/check command

--- bot.py
+++ bot.py
@@ -4,53 +4,49 @@ if __name__ == "__main__":
     load_dotenv()  # Only run this part in dev env
 
 import os
-from telegram import Update, ChatAction, ParseMode
+import logging
+from telegram import ChatAction, ParseMode
 from telegram.ext import CommandHandler, Updater
 
 from data import loadTrainTimingData
 from utils import cleanForMarkdown, getSimilarStations
+
 
 TELEGRAM_TOKEN = os.environ["TELEGRAM_API"]  # Load in Telegram bot token
 TRAIN_TIME_DATA = loadTrainTimingData(
     "train-timing.json"
 )  # Load data from the json file
 
-
-def send_typing_action(func):
-    """Sends typing action while processing func command."""
-
-    def command_func(update, context, *args, **kwargs):
-        context.bot.send_chat_action(
-            chat_id=update.effective_message.chat_id, action=ChatAction.TYPING
-        )
-        return func(update, context, *args, **kwargs)
-
-    return command_func
+log = logging.getLogger()
+log.debug("Starting bot...")
 
 
-def temp_send_typing_action(update, context):
-    context.bot.send_chat_action(
-        chat_id=update.effective_message.chat_id, action=ChatAction.TYPING
-    )
-
-
-@send_typing_action
 def command_queryStation(update, context):
+    log.debug("Query from user %s", update.message.chat.username)
+    log.debug("Message content: [%s]", update.message.text)
+
     queryParams = context.args
     if len(queryParams) == 0:
         update.message.reply_text("Please give me a station name!")
+        log.debug("Ending since query is empty")
         return
     stationName = " ".join(queryParams)
 
     # Handle the case where user input doesn't exactly match
     if stationName not in TRAIN_TIME_DATA:
+        log.debug('"%s" is not in list of station names', stationName)
         similarity = getSimilarStations(stationName, TRAIN_TIME_DATA.values())
 
         stationName = cleanForMarkdown(stationName)
         topSimRatio = similarity[0]["ratio"]
         topSimStation = similarity[0]["station"]
 
+        log.debug(
+            "Top similarity is %s, station is %s", str(topSimRatio), topSimStation.name
+        )
+
         if 0.7 <= topSimRatio < 0.9:
+
             # Not that good, but good enough as the top suggestion
             update.message.reply_text(
                 f"*Couldn't find {stationName}\!* Did you mean *_{topSimStation.getCleanedName()}_*?",
@@ -94,6 +90,8 @@ def command_queryStation(update, context):
         # Good enough match, use this as stationName instead
         stationName = topSimStation.getCleanedName()
 
+    log.debug("Sending out info for %s", stationName)
+
     lineInformation = TRAIN_TIME_DATA[stationName].getLineInformation()
     timingsMessages = TRAIN_TIME_DATA[stationName].getFormattedTimings()
 
@@ -104,15 +102,26 @@ def command_queryStation(update, context):
     )
 
     for eachMsg in timingsMessages:
-        temp_send_typing_action(update, context)
         update.message.reply_text(eachMsg, parse_mode=ParseMode.MARKDOWN_V2)
+
+    log.debug("Query from %s completed", update.message.chat.username)
+
+
+def command_start(update, context):
+    log.debug("Start request from %s", update.message.chat.username)
+    startMsg = "*Hello\! I'm LastTrainBot\.* Just use `/check <station name>` and I'll tell you the last times for that station\."
+    update.message.reply_text(startMsg, parse_mode=ParseMode.MARKDOWN_V2)
+    log.debug("Start request from %s completed", update.message.chat.username)
 
 
 updater = Updater(TELEGRAM_TOKEN)
 dispatcher = updater.dispatcher
 
-dispatcher.add_handler(
-    CommandHandler("start", command_queryStation)
-)  # Making a handler for the type Update
+dispatcher.add_handler(CommandHandler("start", command_start))  # Start command
 
+dispatcher.add_handler(
+    CommandHandler("check", command_queryStation)
+)  # Actual query command
+
+log.debug("Bot started")
 updater.start_polling()
